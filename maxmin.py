335715abc40c16b4b6f18eddbec60c5db17df487
abutez <steveabuta@gmail.com>
2016-11-01 16:51:03 +0300

    a function to identify the min and max in a list

---
+++ maxmin.py
@@ -0,0 +1,13 @@
+#prints the maximum and minimum of a list
+def MaxMin(mylist):
+    #result should be in an array
+    mylist=[]
+    #if minimum and maximum is equal returns the number
+    if max in (mylist)==min(mylist):
+        return len(mylist)
+    #returns the minimum and the maximum
+    else:
+        return [min(mylist), max(mylist)]
+
+
+
